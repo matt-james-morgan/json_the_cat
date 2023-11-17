9018d5c2f645877b73d9c958c23da2a00d03e9b1
Matt Morgan <matt.james.morgan97@gmail.com>
2023-11-17 00:00:29 +0000

    made tests for server requests

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,15 +1,26 @@
 const request = require("request");
-const input = process.argv.slice(2);
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${input}`, (error, response, body)=>{
-  if(error){
-    console.log("Breed not found");
-    return;
-  }
-  
- 
- const data = JSON.parse(body);
 
- console.log(data[0].description);
-})
+const fetchBreedDescription = (breed, callback) =>{
+
+  //uses request library to retrieve json from link
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body)=>{
+
+    
+    const data = JSON.parse(body);
+    
+
+    if (data[0] === undefined) {
+      callback(error, null);
+
+    } else {
+
+      callback(error, data[0].description);
+    }
+    
+
+  });
+};
+
+module.exports = {fetchBreedDescription};
 
